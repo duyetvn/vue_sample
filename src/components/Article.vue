6c105f1fbcb29ee3ce36074827e5446611e03cdf
Duyet Nguyen <duyet.vn@gmail.com>
2019-06-10 09:01:36 +0000

    First Vue app sample

---
+++ src/components/Article.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="article">
+    <h1>{{ article.title }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Article',
+  props: ['article']
+}
+</script>
---
+++ src/components/Articles.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="articles">
+    <ul>
+      <li v-for="article in articles" v-bind:key="article.id">
+        <Article :article="article"></Article>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import Article from './Article'
+export default {
+  name: 'Articles',
+  components: {
+    Article
+  },
+  data () {
+    return {
+      articles: [
+        {id: 1, title: 'Gone with the wind'},
+        {id: 2, title: 'Romeo and Julie'},
+        {id: 3, title: 'Casablanca'}
+      ]
+    }
+  }
+}
+</script>
